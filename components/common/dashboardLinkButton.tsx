12d0f98a066cdd2eb4d7e26d345d70524f8d7795
Danny <danny.hj.vogel@gmail.com>
2025-08-10 11:25:00 +0200

    Feat: add dashboardLinkButton component for user navigation to dashboard

---
+++ components/common/dashboardLinkButton.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { SignIn } from "@/components/auth/signin-button";
+import { auth } from "@/lib/auth"
+
+export default async function dashboardLinkButton() {
+    const session = await auth();
+    if (!session?.user) return null;
+    return (
+        <>
+            {
+                session?.user ? <Button className="mb-4" asChild>
+                    <Link href="/dashboard">Go to Dashboard</Link>
+                </Button> : <SignIn />
+            }
+        </>
+    );
+}
