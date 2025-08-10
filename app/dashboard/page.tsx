7aad89e3e7a7174e83dfcd40e68f29703ceb2c20
Danny <danny.hj.vogel@gmail.com>
2025-08-10 11:25:44 +0200

    Refactor: update ExerciseCard component to enhance functionality and display exercise instructions in an accordion format

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,22 +1,18 @@
 import { Suspense } from 'react';
 import ExerciseCard from "@/components/common/excerciseCard";
-import { fetcher } from "@/lib/api";
-import { SignOut } from '@/components/auth/signout-button';
-import { Username } from '@/components/auth/username';
+import { getExerciseById } from "@/lib/api";
 
 export default function Page() {
-    const fetchPromise = fetcher("example");
+    const fetchPromise = getExerciseById("ztAa1RK");
     return (
         <div>
             <h1>My Dashboard</h1>
             <p>Welcome to your dashboard!</p>
             <Suspense fallback={<div>Loading...</div>}>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-                    <ExerciseCard title="Cardio Workout" description="Improve your cardiovascular health with our cardio exercises." fetchPromise={fetchPromise} />
+                    <ExerciseCard fetchPromise={fetchPromise} />
                 </div>
             </Suspense>
-            <Username />
-            <SignOut />
 
         </div>
     );
