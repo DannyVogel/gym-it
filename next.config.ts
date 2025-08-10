2c7e8f1dc5bf072acc06afac55f1411b4a4cd3ec
Danny <danny.hj.vogel@gmail.com>
2025-08-10 11:24:14 +0200

    Feat: add remotePatterns for image optimization in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [new URL("https://static.exercisedb.dev/**")],
+  },
 };
 
 export default nextConfig;
