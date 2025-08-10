91fd333a894db1bbb4621e0a79f0766d65a7af01
Danny <danny.hj.vogel@gmail.com>
2025-08-10 11:25:32 +0200

    Refactor: update Home component to use DashbordLinkButton and Username for improved navigation and user display

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,15 @@
-import Link from 'next/link'
-import { Button } from "@/components/ui/button"
-import { SignIn } from "@/components/auth/signin-button"
+import DashbordLinkButton from "@/components/common/dashboardLinkButton"
 import { AuthWarn } from "@/components/auth/auth-warn";
+import { Username } from '@/components/auth/username';
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="flex flex-col items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold mb-4">Welcome to Gym IT</h1>
       <p className="text-lg mb-8">Your personal fitness management system.</p>
-      <Button className="mb-4" asChild>
-        <Link href="/dashboard">Go to Dashboard</Link>
-      </Button>
+      <Username />
+      <DashbordLinkButton />
       <AuthWarn />
-      <SignIn />
-
     </div>
   );
 }
