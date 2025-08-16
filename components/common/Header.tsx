728cc302536d32a45a0998792dd4afa2ae784cbe
DannyVogel <danny.hj.vogel@gmail.com>
2025-08-16 12:03:07 +0300

    Fix: show nav links if logged in

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,20 +1,28 @@
 import Link from "next/link";
-import { SignOut } from '@/components/auth/signout-button';
+import { SignOut } from "@/components/auth/signout-button";
+import { auth } from "@/lib/auth";
 
-export default function Header() {
-    return (
-        <div className="flex items-center justify-between p-4 bg-gray-800 text-white">
-            <Link href="/" className="text-xl font-bold">Gym IT</Link>
-            <div className="flex items-center space-x-4">
-                <nav>
-                    <ul className="flex space-x-4">
-                        <li>
-                            <a href="/dashboard" className="hover:underline">Dashboard</a>
-                        </li>
-                    </ul>
-                </nav>
-                <SignOut />
-            </div>
+export default async function Header() {
+  const session = await auth();
+  return (
+    <div className="flex items-center justify-between p-4 bg-gray-800 text-white">
+      <Link href="/" className="text-xl font-bold">
+        Gym IT
+      </Link>
+      {session?.user ? (
+        <div className="flex items-center space-x-4">
+          <nav>
+            <ul className="flex space-x-4">
+              <li>
+                <a href="/dashboard" className="hover:underline">
+                  Dashboard
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <SignOut />
         </div>
-    );
+      ) : null}
+    </div>
+  );
 }
