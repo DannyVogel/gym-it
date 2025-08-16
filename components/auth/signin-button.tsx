b5474051a1906ea0c36f4d533834984da90907e1
DannyVogel <danny.hj.vogel@gmail.com>
2025-08-16 12:03:21 +0300

    Fix: format code in SignIn component for consistency

--- components/auth/signin-button.tsx
+++ components/auth/signin-button.tsx
@@ -1,15 +1,15 @@
-import { signIn } from "@/lib/auth"
-import { Button } from "@/components/ui/button"
+import { signIn } from "@/lib/auth";
+import { Button } from "@/components/ui/button";
 
 export function SignIn() {
-    return (
-        <form
-            action={async () => {
-                "use server"
-                await signIn("google", { redirectTo: "/dashboard" })
-            }}
-        >
-            <Button type="submit">Sign in</Button>
-        </form>
-    )
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn("google", { redirectTo: "/dashboard" });
+      }}
+    >
+      <Button type="submit">Sign in</Button>
+    </form>
+  );
 }
